5840c5d01658cec68b193c448297a1656518a608
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2023-04-12 15:51:56 +0200

    Add defaults for variables

--- variables.tf
+++ variables.tf
@@ -4,11 +4,13 @@
 variable "location" {
   description = "Location of the network"
   type        = string
+  default     = "WestEurope"
 }
 
 variable "vnet_prefix" {
   description = "Prefix of the created VNET. It will be used as a base name for the created VNET, Resource Group, etc."
   type        = string
+  default     = "cf" 
 }
 
 variable "vnet_size" {
@@ -17,7 +19,7 @@ variable "vnet_size" {
 }
 
 variable "address_space_workload" {
-  description = "The address space in CIDR notation for your workload subnets."
+  description = "The address space in CIDR notation for your workload subnets. For example 10.1.0.0/25"
   type        = string
   validation {
     condition     = can(cidrnetmask(var.address_space_workload))
